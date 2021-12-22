bd53606d5380e9035f5cf28e505d81983a0904e3
miloszmisiek <milosz.misiek@gmail.com>
2021-12-22 14:50:45 +0000

    Add start, restart and help button with draft styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 /**  After DOM finis loading, gameboard is drawn by
  * adding respective side of the border based on index number. 
  */
- document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     let fields = document.getElementsByClassName("field");
 
     for (let i = 0; i < fields.length; i++) {
+
         if (i < 3) {
             fields[i].style.borderBottom = "1px solid var(--gray)";
         };
@@ -17,6 +18,11 @@
         };
         if (i % 3 === 2) {
             fields[i].style.borderLeft = "1px solid var(--gray)";
-        }
+        };
+        fields[i].addEventListener("click", fieldClicked);
     }
-})
+});
+
+function fieldClicked(event) {
+    console.log("clicked");
+}
